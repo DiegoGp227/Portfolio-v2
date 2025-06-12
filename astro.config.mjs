6b568fd42f6e7b27431d90c9a6b6902936ed9935
DiegoGp227 <diego.gp227@gmail.com>
2025-06-11 23:42:35 -0500

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import path from 'path';
+
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./puclic'), 
+      },
+    },
+  },
+});
